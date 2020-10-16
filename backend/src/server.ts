0de7e385a5d2d27dcac8759bdf0087bdb332c3c2
welitoncardozo <welitoncardozo.br@gmail.com>
2020-10-16 13:37:55 -0300

    Refactored camel case. Removed database.sqlite from git.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,16 +3,16 @@ import 'express-async-errors';
 import path from 'path';
 import cors from 'cors';
 
-import './database/connection';
-import routes from './routes';
-import HandleError from './errors/HandleError';
+import './database/Connection';
+import routes from './Routes';
+import handleError from './errors/HandleError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(HandleError);
+app.use(handleError);
 
 const uploadsDirectory = path.join(__dirname, '..', 'uploads');
 app.use('/uploads', express.static(uploadsDirectory));
